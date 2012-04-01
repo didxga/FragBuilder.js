71880b58d9e3d4d49a468faeed0d0c9b21a61c1e
rlemon <rob.lemon@gmail.com>
2012-04-01 16:08:16 -0400

    fix for empty textContent, added two options; .json(jsonFragment), and .string(HTMLstring)

--- FragBuilder.js
+++ FragBuilder.js
@@ -4,39 +4,53 @@ var FragBuilder = (function() {
             element.style[prop] = style_object[prop];
         }
     };
-    var generateFragment = function(json) {
+    var generateFragmentFromJSON = function(json) {
         var tree = document.createDocumentFragment();
         json.forEach(function(obj) {
-            if (!('tagName' in obj)) {
-                throw "Error: Malformed Fragment; 'tagName' must exist for each element.";
-                return;
-            }
-            var el = document.createElement(obj.tagName);
-            delete obj.tagName;
-            for (part in obj) {
-                var val = obj[part];
-                switch (part) {
-                case ('textContent'):
-                    el.appendChild(document.createTextNode(val));
-                    break;
-                case ('style'):
-                    applyStyles(el, val);
-                    break;
-                case ('className'):
-                    el.className = val;
-                    el.setAttribute('class', val);
-                    break;
-                case ('childNodes'):
-                    el.appendChild(generateFragment(val));
-                    break;
-                default:
-                    el.setAttribute(part, val);
-                    break;
+            if (!('tagName' in obj) && 'textContent' in obj) {
+                tree.appendChild(document.createTextNode(obj['textContent']));
+            } else if ('tagName' in obj) {
+                var el = document.createElement(obj.tagName);
+                delete obj.tagName;
+                for (part in obj) {
+                    var val = obj[part];
+                    switch (part) {
+                    case ('textContent'):
+                        el.appendChild(document.createTextNode(val));
+                        break;
+                    case ('style'):
+                        applyStyles(el, val);
+                        break;
+                    case ('className'):
+                        el.className = val;
+                        el.setAttribute('class', val);
+                        break;
+                    case ('childNodes'):
+                        el.appendChild(generateFragmentFromJSON(val));
+                        break;
+                    default:
+                        el.setAttribute(part, val);
+                        break;
+                    }
                 }
+                tree.appendChild(el);
+            } else {
+                throw "Error: Malformed Fragment";
             }
-            tree.appendChild(el);
         });
         return tree;
     };
-    return generateFragment;
+    var generateFragmentFromString = function(HTMLstring) {
+        var div = document.createElement("div"),
+            tree = document.createDocumentFragment();
+        div.innerHTML = HTMLstring;
+        while (div.hasChildNodes()) {
+            tree.appendChild(div.firstChild);
+        }
+        return tree;
+    };
+    return {
+        'json': generateFragmentFromJSON,
+        'string': generateFragmentFromString
+    };
 }());
